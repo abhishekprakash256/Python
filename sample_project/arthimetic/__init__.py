65fd033b8abb3cc074f7c3959b2934cf2db2c899
abhi <abhishekprakash256@gmail.com>
2022-01-26 12:42:53 -0500

    imports

---
+++ sample_project/arthimetic/__init__.py
@@ -0,0 +1,13 @@
+"""
+The import file 
+"""
+
+from arthimetic.add import *
+
+from arthimetic.sub import *
+
+from arthimetic.mul import *
+
+from arthimetic.divide import *
+
+from arthimetic.mod import *
--- sample_project/main.py
+++ sample_project/main.py
@@ -1,4 +1,12 @@
 """
 The main file of the project
+calls all the function and performs all the functionality
 """
 
+import arthimetic
+
+print(arthimetic.add(2,2))
+
+print(arthimetic.sub(4,1))
+
+print(arthimetic.mul(3,3))
