871a432f7c8790ba55396e39fd3948d01d5b64b5
abhishekprakash256 <abhishekprakash47@gmail.com>
2022-07-26 00:58:14 -0400

    Python list

--- data_structure_and_algo/linked_list/linked_list.py
+++ data_structure_and_algo/linked_list/linked_list.py
@@ -1,5 +1,46 @@
 """
-make the linked list of the in the python
-
+make the singly linked list of the in the python
+from the scrath implementation 
 """
+
+class linked_list():
+	"""
+	The linked list class to make the list 
+	with using the nodes 
+	"""
+	def __init__(self,value,next =None):
+		self.value = value
+		self.next = next
+
+	def itertator(self,node):
+		self.node = node
+		cur = self.node
+		while True:
+			if cur.next is None:
+				print(cur.value)
+				break
+			print(cur.value)
+			cur = cur.next
+
+
+
+
+
+#make a simple linked list 
+
+
+#making the connection in the two 
+
+
+
+
+node1 = linked_list(5)
+node2 = linked_list(6)
+node3 = linked_list(7)
+node4 = linked_list(8)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+
 
