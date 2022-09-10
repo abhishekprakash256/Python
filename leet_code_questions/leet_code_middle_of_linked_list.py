62fef0f5b4a65fec6f015e144f482e6968d21652
abhishek <abhishekprakash47@gmail.com>
2022-09-10 15:06:25 -0400

    files

---
+++ leet_code_questions/leet_code_middle_of_linked_list.py
@@ -0,0 +1,76 @@
+"""
+The question is to find the middle of the linked list
+"""
+
+#test cases
+
+
+Input1 = [1,2,3,4,5]
+Output1 = [3,4,5]
+
+
+#Definition for singly-linked list.
+class ListNode:
+	def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+
+
+
+
+class Solution:
+    def middleNode(self, head):
+    	"""
+    	The function returns the middle of the linked list 
+		Args:
+			head (node) : the head node of the linked list
+		Returns:
+			middle_node_val (node): the middle node of the linked list or the second node 
+    	"""
+
+    	temp = head
+    	
+    	count =0
+
+    	#get the length of the linked list 
+    	while True:
+    		if temp.next  == None:
+    			count+=1
+    			break
+
+    		count+=1
+    		temp = temp.next
+
+    	#the check of the length
+    	if count %2 == 0:
+    		index = (count/2)+1
+    	else:
+    		index = (count+1)/2
+
+    	print(index)
+    	pointer = 0
+    	while pointer <=index+1:
+    		pointer +=1
+    		temp1 = head
+    		temp1 = temp1.next
+
+    		
+
+    	return temp1.val
+
+if __name__ == '__main__':
+	#to make the linked list
+	node1 = ListNode(1)
+	node2 = ListNode(2)
+	node3 = ListNode(3)
+	node4 = ListNode(4)
+	node5 = ListNode(5)
+
+	node1.next = node2
+	node2.next = node3
+	node3.next = node4
+	node4.next = node5
+
+	Soln = Solution()
+	res = Soln.middleNode(node1)
+	print(res)
