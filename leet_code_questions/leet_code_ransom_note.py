cada546be979e862528e0dcc6aa50a84bfd7033d
abhishek <abhishekprakash47@gmail.com>
2022-09-10 17:04:31 -0400

    questions

--- leet_code_questions/leet_code_ransom_note.py
+++ leet_code_questions/leet_code_ransom_note.py
@@ -6,31 +6,34 @@ if the string can be construted using the other string
 #test case 
 ransomNote = "a"
 magazine = "b"
-Output = false
+Output = False
 
 ransomNote2 = "aa"
 magazine2 = "ab"
-Output2 = false
+Output2 = False
 
 ransomNote3 = "aa"
 magazine3 = "aab"
-Output3 = true
+Output3 = True
 
 
 class Solution:
-    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        """
-        The function to find the similar string in the two strings
-        Args: 
-        	ransomNOte (str) : the first string 
-        	magazine (str) : the secomd string 
- 		Returns:
- 			true or false (bool) : the value if corret or not 
-        """
+	def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+		"""
+		The function to find the similar string in the two strings
+		Args: 
+			ransomNOte (str) : the first string 
+			magazine (str) : the secomd string 
+		Returns:
+			true or false (bool) : the value if corret or not 
+		"""
+		if ransomNote in magazine:
+			return True
+		else:
+			return False
+		
 
-        for i in ransomNote:
-        	if i not in magazine:
-        		return False
-
-        	return True
-        
+if __name__ == '__main__':
+	soln = Solution()
+	res = soln.canConstruct(ransomNote3, magazine3)
+	print(res)
