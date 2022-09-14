c037c777b8de09b7beffb9d8d80543b4e5ff51da
abhishek <abhishekprakash47@gmail.com>
2022-09-13 22:46:19 -0400

    questions

--- leet_code_questions/array_101/leet_code_array_sqr_sorted.py
+++ leet_code_questions/array_101/leet_code_array_sqr_sorted.py
@@ -14,23 +14,22 @@ Output2 = [4,9,9,49,121]
 
 
 class Solution:
-    def sortedSquares(self, nums: int) -> int:
-        """
-        The function makes the square of the list numbers
-        and sort the array
-        Args:
-        	nums(list) : the list of the integers 
-        Returns:
-        	squared_list (list) : the sorted list of the squaures of the num
-        """
-        squared_lst = []
-        for i in nums:
-        	print(i)
-        	squared_lst.append(i*i)
+	def sortedSquares(self, nums: int) -> int:
+		"""
+		The function makes the square of the list numbers
+		and sort the array
+		Args:
+			nums(list) : the list of the integers 
+		Returns:
+			squared_list (list) : the sorted list of the squaures of the num
+		"""
+		squared_lst = []
+		for i in nums:
+			squared_lst.append(i*i)
 
-        squared_lst.sort()
+		squared_lst.sort()
 
-        return squared_lst
+		return squared_lst
 
 if __name__ == '__main__':
 	Soln = Solution()
