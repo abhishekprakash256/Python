6a990517f5d3a5b7fb29aaf9d152207a360db443
abhishek <abhishekprakash47@gmail.com>
2022-12-17 13:07:43 -0500

    stack implementation queue

--- udacity_data_structures_and_algo/stacks/python_stack.py
+++ udacity_data_structures_and_algo/stacks/python_stack.py
@@ -3,11 +3,40 @@ using the inbuild python stack as the list
 
 """
 
-stack = []
+stack0 = []
 
 
-stack.append('a')
-stack.append('b')
-stack.append('c')
+#push operation
+stack0.append('a')
+stack0.append('b')
+stack0.append('c')
 
-print(stack)
+#after the push operation
+print(stack0)
+
+#pop operation 
+stack0.pop()
+
+
+#after the pop operation
+print(stack0)
+
+
+
+#impelemtation using the collections 
+
+from collections import deque
+
+stack1 = deque()
+
+#push the eleements
+stack1.append(1)
+stack1.append(2)
+stack1.append(3)
+
+#pop the elements in the queue
+stack1.pop()
+stack1.pop()
+
+#print after the pop
+print(stack1)
