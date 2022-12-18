46dc1c58396d51ac95fa73608d28efe805877a8c
abhishek <abhishekprakash47@gmail.com>
2022-12-18 18:17:51 -0500

    new question

--- leet_code_questions/array_101/leet_code_check_double_exists.py
+++ leet_code_questions/array_101/leet_code_check_double_exists.py
@@ -7,9 +7,10 @@ arr = [10,2,5,3]
 
 arr2 = [3,1,7,11]
 
+arr3 = [7,1,14,11]
 
 class Solution:
-	def CheckIfExists(self,arr):
+	def checkIfExist(self,arr):
 		"""
 		The given function checks whether the given array has the double or not 
 		Args:
@@ -17,19 +18,15 @@ class Solution:
 		Returns:
 			True or False : (Bool) on the value that exists or not
 		"""
-		double_hash = {}
-
-		for i in arr:
-			double_val = 2*i
-			if double_val not in double_hash:
-				double_hash.update({i:True})
-			else:
+		s = set()
+		for num in arr:
+			if float(num) / 2 in s or num * 2 in s:
 				return True
-				
+			s.add(num)
 		return False
 
 
 if __name__ == '__main__':
 	sol = Solution()
-	res = sol.CheckIfExists(arr2)
+	res = sol.checkIfExist(arr3)
 	print(res)
