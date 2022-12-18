0d92ec9073ce1c129054e4837b9a38e36c1f81b2
abhishekprakash256 <abhishekprakash47@gmail.com>
2022-12-17 22:59:49 -0500

    stack scratch

--- udacity_data_structures_and_algo/stacks/stack_scratch.py
+++ udacity_data_structures_and_algo/stacks/stack_scratch.py
@@ -18,26 +18,46 @@ class Stack:
 		if self.head is None:
 			self.head = node
 		else:
-			node.next = self.head.next
+			node.next = self.head
 			self.head = node
+
+	def pop(self):
+		temp = self.head
+		temp_head  = self.head
+		temp.next = self.head
+		temp_head.next = None
 
 
 	def print_stack(self):
 		temp = self.head
 		
+		print("The stack is -->")
+
 		while True:
+			print(temp.data)
 			if temp.next is None:
 				break
-			print(temp.data)
 			temp = temp.next
+
+
 
 
 #make the stack
 
 my_stack = Stack()
 
+
+#push the elements 
+my_stack.push(1)
 my_stack.push(2)
 my_stack.push(3)
 my_stack.push(4)
+my_stack.push(5)
+
+
+#pop the elements 
+
+my_stack.pop()
+
 
 my_stack.print_stack()
