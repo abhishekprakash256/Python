95cd7947f561248e8611ece3f26ccb637a475f8b
abhishekprakash256 <abhishekprakash47@gmail.com>
2022-12-30 22:37:07 -0500

    merge sort code

--- udacity_data_structures_and_algo/searching_sorting/merge_sort.py
+++ udacity_data_structures_and_algo/searching_sorting/merge_sort.py
@@ -27,12 +27,61 @@ def divide_lst(nums:list)->list:
 		divide_lst(nums[0:mid])
 		divide_lst(nums[mid:length])
 
+lst2 = [1,1,2,3]
+
+lst3 = [3,4,5,6,6]
+
+lst4 = [1,1]
+lst5 = [2,2,3]
 
 
- 
+def join_lst(nums1:list, nums2:list) ->list:
+	"""
+	The algorithm to join the list in the sorted manner 
+	Args:
+		nums1: (list) The list of the integers
+		nums2: (list) The list of the interges
+	Return:
+		combined_lst: (list) the combined sorted list 
+	"""
+	if len(nums1) < len(nums2):
+		temp = nums1
+		nums1 = nums2
+		nums2 = temp
 
+	combined_lst = []
+	i,j,k = 0,0,0
+	length = len(nums1) + len(nums2) - 1
+	print(length)
 
+	while k < length:
+		if i == len(nums1):
+			combined_lst.append(nums2[j])
+			j+=1
+		
+		elif j == len(nums2) :
+			combined_lst.append(nums1[i])
+			i+=1
 
+		else:
+			print("inside else")
+			if nums1[i] == nums2[j]:
+				combined_lst.append(nums1[i])
+				combined_lst.append(nums2[j])
+				i+=1
+				j+=1
+			elif nums1[i] < nums2[j]:
+				print("one")
+				combined_lst.append(nums1[i])
+				i+=1
+			elif nums1[i] > nums2[j]:
+				print("two")
+				combined_lst.append(nums2[j])
+				j+=1
+
+		k+=1
+
+	return combined_lst 
 
 
 def merge_sort(nums:list):
@@ -46,5 +95,6 @@ def merge_sort(nums:list):
 	pass
 
 
-print(divide_lst(lst))
+#print(divide_lst(lst))
 	
+print(join_lst(lst4,lst5))
