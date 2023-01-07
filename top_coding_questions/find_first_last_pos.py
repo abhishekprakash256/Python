abb7d17b5f2eebca423b78277cdd417a91eaad02
abhishekprakash256 <abhishekprakash47@gmail.com>
2023-01-07 01:07:25 -0500

    position code

--- top_coding_questions/find_first_last_pos.py
+++ top_coding_questions/find_first_last_pos.py
@@ -4,7 +4,20 @@ return [-1,-1] if the value is not in the array or return [first_pos, last_pos]
 """
 
 #test cases 
+inp = [1,2,3,4,5,5,5,5,6]
+target = 5
+out = [4,7]
 
+inp1 = [1,4,6,23]
+target1 = 5
+out1 = [-1.-1] 
+
+inp2 = []
+target2 = 4
+out2 = [-1,-1]
+
+
+#working solution
 
 def find_pos(nums:int, arr:list)->list:
 	"""
@@ -15,4 +28,27 @@ def find_pos(nums:int, arr:list)->list:
 	Return:
 		pos_list: (list) The first and the last index of the value in the list
 	"""
-	
+	mapper = {}
+	pos_lst = []
+
+	if len(arr) == 0:
+		return [-1,-1]
+
+	for i in range(len(arr)-1):
+		if arr[i] not in mapper and arr[i] == nums:
+			mapper[arr[i]] = i
+			pos_lst.append(i)
+		mapper[arr[i]] = i
+
+	if nums not in mapper.keys():
+		return [-1,-1]
+	pos_lst.append(mapper[nums])
+	return pos_lst
+
+
+
+
+
+if __name__ == '__main__':
+	res = find_pos(target2,inp2)
+	print(res)
