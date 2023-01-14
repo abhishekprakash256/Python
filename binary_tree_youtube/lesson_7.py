2d4af5d96f94f5b6e1da2bd50951d65771c2102c
abhishekprakash256 <abhishekprakash47@gmail.com>
2023-01-13 22:00:03 -0500

    sum problem

--- binary_tree_youtube/lesson_7.py
+++ binary_tree_youtube/lesson_7.py
@@ -62,6 +62,39 @@ def tree_sum_recursive(root)->sum:
 	elif root.right is not None and root.left is None:
 		return tree_sum_recursive(root.right) + root.data
 
+
+def tree_sum_intertive_dfs(root)->sum:
+	"""
+	The program to find the sum of the all nodes of the tree
+	Args:
+		root: (node) The root node of the tree
+	Returns:
+		sum: (int) The sum of the nodes of the tree 
+	"""
+	stack = []
+	stack.append(root)
+	sum = 0
+
+	while len(stack) !=0:
+
+		temp = stack.pop()
+		sum = temp.data + sum
+
+		if temp.right is not None and temp.left is not None:
+			stack.append(temp.right)
+			stack.append(temp.left)
+		elif temp.right is None and temp.left is not None:
+			stack.append(temp.left)
+		elif temp.left is None and temp.right is not None:
+			stack.append(temp.right)
+
+
+	return sum
+
+
+
 if __name__ == '__main__':
-	res = tree_sum_recursive(root)
-	print(res)
+	res0 = tree_sum_recursive(root)
+	res1 = tree_sum_intertive_dfs(root) 
+	print(res0)
+	print(res1)
