423cba9b3e50e9659fca44664844806445023944
abhishekprakash256 <abhishekprakash47@gmail.com>
2023-06-15 06:45:14 -0400

    push code

--- leet_code_questions/neetcode_map/Trees/leet_code_sum_tree.py
+++ leet_code_questions/neetcode_map/Trees/leet_code_sum_tree.py
@@ -2,3 +2,46 @@
 to find the sum of the tree 
 """
 
+#make the node 
+class Node:
+	def __init__(self,val):
+		self.val = val 
+		self.left = None
+		self.right = None
+
+
+
+
+class Solution:
+	def sum_tree(self,root):
+		"""
+		Find the sumo of the tree
+		"""
+
+		#make the recursive soltion 
+
+		if root is None:
+			return 0 
+
+		return root.val + self.sum_tree(root.left) + self.sum_tree(root.right)
+
+
+
+
+
+if __name__ == "__main__":
+	root = Node(1)
+	node1 = Node(2)
+	node2 = Node(3)
+	node3 = Node(4)
+
+	#connect the node
+
+	root.left = node1 
+	root.right = node2
+	node2.right = node3
+
+
+	sol = Solution()
+	sum = sol.sum_tree(root)
+	print(sum)
